ea12fa0c537099247261b32133b16f4361689b1a
Liu-Steve <steve_liu@whu.edu.cn>
2022-10-30 17:20:15 +0800

    20221028改

--- 20221028-907.cpp
+++ 20221028-907.cpp
@@ -5,38 +5,42 @@
     cout.tie(0)
 using namespace std;
 
+typedef pair<int, int> p;
+const int MOD = 1000000007;
+
 class Solution {
 public:
-    int sumSubarrayMins(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> monoStack;
-        vector<int> left(n), right(n);
-        for (int i = 0; i < n; i++) {
-            while (!monoStack.empty() && arr[i] <= arr[monoStack.back()]) {
-                monoStack.pop_back();
+    int sumSubarrayMins(const vector<int>& arr)
+    {
+        int n = arr.size(), st_len = 0, ret = 0;
+        p st[n];
+        int dp[n];
+        for (int i = 0; i < n; ++i) {
+            int j;
+            while (st_len && st[st_len - 1].first > arr[i])
+                st_len--;
+            if (!st_len) {
+                j = -1;
+                dp[i] = arr[i] * (i - j);
+            } else {
+                j = st[st_len - 1].second;
+                dp[i] = arr[i] * (i - j) + dp[j];
             }
-            left[i] = i - (monoStack.empty() ? -1 : monoStack.back());
-            monoStack.emplace_back(i);
-        }
-        monoStack.clear();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!monoStack.empty() && arr[i] < arr[monoStack.back()]) {
-                monoStack.pop_back();
-            }
-            right[i] = (monoStack.empty() ? n : monoStack.back()) - i;
-            monoStack.emplace_back(i);
-        }
-        long long ret = 0;
-        long long mod = 1e9 + 7;
-        for (int i = 0; i < n; i++) {
-            ret = (ret + (long long)left[i] * right[i] * arr[i]) % mod; 
+            st[st_len].first = arr[i];
+            st[st_len].second = i;
+            st_len++;
+            ret += dp[i];
+            ret %= MOD;
         }
         return ret;
     }
 };
 
-int main() {
+int main()
+{
     io;
     Solution s;
+    vector<int> arr = {11,81,94,43,3};
+    cout << s.sumSubarrayMins(arr) << endl;
     return 0;
 }
