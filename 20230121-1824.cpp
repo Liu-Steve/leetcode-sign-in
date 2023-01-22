68a604c56b36043e36e05022563ad72c68327790
Liu-Steve <steve_liu@whu.edu.cn>
2023-01-23 01:24:32 +0800

    20230121

---
+++ 20230121-1824.cpp
@@ -0,0 +1,47 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vector<int>> vv;
+typedef pair<ll, ll> pr;
+
+const ll MOD = 1000000007;
+
+class Solution {
+    static constexpr int inf = 0x3f3f3f3f;
+public:
+    int minSideJumps(vector<int> &obstacles) {
+        int n = obstacles.size() - 1;
+        vector<int> d = {1, 0, 1};
+        for (int i = 1; i <= n; i++) {
+            int minCnt = inf;
+            for (int j = 0; j < 3; j++) {
+                if (j == obstacles[i] - 1) {
+                    d[j] = inf;
+                } else {
+                    minCnt = min(minCnt, d[j]);
+                }
+            }
+            for (int j = 0; j < 3; j++) {
+                if (j == obstacles[i] - 1) {
+                    continue;
+                }
+                d[j] = min(d[j], minCnt + 1);
+            }
+        }
+        return *min_element(d.begin(), d.end());
+    }
+};
+
+int main()
+{
+    io;
+    Solution s;
+    return 0;
+}
