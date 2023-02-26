8b3c21ba4bdefb4fca67d1009bc9569edd5280d8
Liu-Steve <steve_liu@whu.edu.cn>
2023-02-27 01:17:56 +0800

    20230226

--- 20230226-1255.cpp
+++ 20230226-1255.cpp
@@ -9,41 +9,60 @@ using namespace std;
 
 typedef long long ll;
 typedef vector<int> vi;
+typedef vector<char> vc;
 typedef vector<string> vs;
 typedef vector<vi> vv;
 typedef pair<double, int> pr;
 typedef vector<pr> vp;
 typedef unordered_set<int> hm;
 
-class Solution {
+class Solution
+{
 public:
-    int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
-        int n = words.size(), res = 0;
-        vector<int> count(26);
-        for (auto c : letters) {
-            count[c - 'a']++;
+    int maxScoreWords(vs &w, vc &l, vi &s)
+    {
+        int ans = 0;
+        int left[26];
+        int val[w.size()];
+        memset(left, 0, sizeof(left));
+        for (char ch : l)
+            ++left[ch - 'a'];
+        for (int i = 0; i < w.size(); ++i)
+        {
+            val[i] = 0;
+            for (char ch : w[i])
+                val[i] += s[ch - 'a'];
         }
-        for (int s = 1; s < (1 << n); s++) {
-            vector<int> wordCount(26); // 统计子集 s 所有单词的字母数目
-            for (int k = 0; k < n; k++) {
-                if ((s & (1 << k)) == 0) { // words[k] 不在子集 s 中
-                    continue;
+
+        function<void(int, int)> dfs = [&](int i, int total)
+        {
+            if (i < 0)
+            {
+                ans = max(ans, total);
+                return;
+            }
+            dfs(i - 1, total);
+            int len = w[i].size();
+            for (int j = 0; j < len; ++j)
+            {
+                --left[w[i][j] - 'a'];
+                if (left[w[i][j] - 'a'] < 0)
+                {
+                    len = j;
+                    break;
                 }
-                for (auto c : words[k]) {
-                    wordCount[c - 'a']++;
-                }
             }
-            bool ok = true; // 判断子集 s 是否合法
-            int sum = 0; // 保存子集 s 的得分
-            for (int i = 0; i < 26; i++) {
-                sum += score[i] * wordCount[i];
-                ok = ok && (wordCount[i] <= count[i]);
+            if (len == w[i].size())
+            {
+                dfs(i - 1, total + val[i]);
+                len--;
             }
-            if (ok) {
-                res = max(res, sum);
-            }
-        }
-        return res;
+            for (int j = 0; j <= len; ++j)
+                ++left[w[i][j] - 'a'];
+        };
+
+        dfs(w.size() - 1, 0);
+        return ans;
     }
 };
 
@@ -51,5 +70,9 @@ int main()
 {
     io;
     Solution s;
+    vs v1 = {"dog", "cat", "dad", "good"};
+    vc v2 = {'a', 'a', 'c', 'd', 'd', 'd', 'g', 'o', 'o'};
+    vi v3 = {1, 0, 9, 5, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    cout << s.maxScoreWords(v1, v2, v3) << endl;
     return 0;
 }
