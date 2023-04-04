f41dfae44c6ef9e600eba924fa59b8be830cea17
Liu-Steve <steve_liu@whu.edu.cn>
2023-04-05 00:53:25 +0800

    20230404

--- 20230404-1000.cpp
+++ 20230404-1000.cpp
@@ -17,31 +17,43 @@ typedef vector<pr> vp;
 typedef unordered_set<int> hm;
 typedef unordered_map<int, int> memory;
 
-class Solution {
+inline int h(int l, int r)
+{
+    return (l << 5) | r;
+}
+
+class Solution
+{
 public:
-    int mergeStones(vector<int> &stones, int k) {
+    int mergeStones(vector<int> &stones, int k)
+    {
         int n = stones.size();
-        if ((n - 1) % (k - 1)) // 无法合并成一堆
+        if ((n - 1) % (k - 1))
             return -1;
+        vector<int> sum(n + 1);
+        sum[0] = 0;
+        for (int i = 0; i < n; ++i)
+            sum[i + 1] = sum[i] + stones[i];
 
-        int s[n + 1];
-        s[0] = 0;
-        for (int i = 0; i < n; i++)
-            s[i + 1] = s[i] + stones[i]; // 前缀和
-
-        int memo[n][n][k + 1];
-        memset(memo, -1, sizeof(memo)); // -1 表示还没有计算过
-        function<int(int, int, int)> dfs = [&](int i, int j, int p) -> int {
-            int &res = memo[i][j][p]; // 注意这里是引用，下面会直接修改 memo[i][j][p]
-            if (res != -1) return res;
-            if (p == 1) // 合并成一堆
-                return res = i == j ? 0 : dfs(i, j, k) + s[j + 1] - s[i];
-            res = INT_MAX;
-            for (int m = i; m < j; m += k - 1) // 枚举哪些石头堆合并成第一堆
-                res = min(res, dfs(i, m, 1) + dfs(m + 1, j, p - 1));
-            return res;
+        memory m;
+        function<int(int, int)> f = [&](int l, int r)
+        {
+            if (l == r)
+                return 0;
+            int hs = h(l, r);
+            if (m.count(hs))
+                return m[hs];
+            int minv = INT32_MAX;
+            for (int p = l; p < r; p += k - 1)
+                minv = min(minv, f(l, p) + f(p + 1, r));
+            if ((r - l) % (k - 1) == 0)
+                minv += sum[r + 1] - sum[l];
+            m[hs] = minv;
+            return minv;
         };
-        return dfs(0, n - 1, 1);
+
+        int ans = f(0, n - 1);
+        return ans;
     }
 };
 
@@ -49,5 +61,7 @@ int main()
 {
     io;
     Solution s;
+    vi v1 = {3, 5, 1, 2, 6};
+    cout << s.mergeStones(v1, 3) << endl;
     return 0;
 }
