adf7c49f6e39b85ee2163d8a14d1d3d297d34e71
Liu-Steve <steve_liu@whu.edu.cn>
2023-04-11 23:54:47 +0800

    20230411

---
+++ 20230411-1041.cpp
@@ -0,0 +1,47 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        vector<vector<int>> direc {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int direcIndex = 0;
+        int x = 0, y = 0;
+        for (char instruction : instructions) {
+            if (instruction == 'G') {
+                x += direc[direcIndex][0];
+                y += direc[direcIndex][1];
+            } else if (instruction == 'L') {
+                direcIndex += 3;
+                direcIndex %= 4;
+            } else {
+                direcIndex++;
+                direcIndex %= 4;
+            }
+        }
+        return direcIndex != 0 || (x == 0 && y == 0);
+    }
+};
+
+int main()
+{
+    io;
+    Solution s;
+    return 0;
+}
