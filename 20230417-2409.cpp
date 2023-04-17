b083a4580b6eba804a4043bc64dc863a4e30ff56
Liu-Steve <steve_liu@whu.edu.cn>
2023-04-17 23:40:47 +0800

    20230417

---
+++ 20230417-2409.cpp
@@ -0,0 +1,49 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution {
+public:
+    int countDaysTogether(string arriveAlice, string leaveAlice, string arriveBob, string leaveBob) {
+        vector<int> datesOfMonths = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        vector<int> prefixSum(1, 0);
+        for (int date : datesOfMonths) {
+            prefixSum.emplace_back(prefixSum.back() + date);
+        }
+
+        int arriveAliceDay = calculateDayOfYear(arriveAlice, prefixSum);
+        int leaveAliceDay = calculateDayOfYear(leaveAlice, prefixSum);
+        int arriveBobDay = calculateDayOfYear(arriveBob, prefixSum);
+        int leaveBobDay = calculateDayOfYear(leaveBob, prefixSum);
+        return max(0, min(leaveAliceDay, leaveBobDay) - max(arriveAliceDay, arriveBobDay) + 1);
+    }
+
+    int calculateDayOfYear(string day, const vector<int> &prefixSum) {
+        int month = stoi(day.substr(0, 2));
+        int date = stoi(day.substr(3));
+        return prefixSum[month - 1] + date;
+    }
+};
+
+int main()
+{
+    io;
+    Solution s;
+    return 0;
+}
