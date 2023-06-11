fc2d4f19411578d3339a2b171c97b2f41d71fbeb
Liu-Steve <steve_liu@whu.edu.cn>
2023-06-11 23:53:25 +0800

    20230611

---
+++ 20230611-1171.cpp
@@ -0,0 +1,55 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        int prefix = 0;
+        unordered_map<int, ListNode*> seen;
+        for (ListNode* node = dummy; node; node = node->next) {
+            prefix += node->val;
+            seen[prefix] = node;
+        }
+        prefix = 0;
+        for (ListNode* node = dummy; node; node = node->next) {
+            prefix += node->val;
+            node->next = seen[prefix]->next;
+        }
+        return dummy->next;
+    }
+};
+
+int main()
+{
+    io;
+    Solution s;
+    return 0;
+}
