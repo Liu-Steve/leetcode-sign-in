08529a6986b89178b5a8544a76834335a39c7a0a
Liu-Steve <steve_liu@whu.edu.cn>
2023-06-17 00:17:02 +0800

    20230617

---
+++ 20230617-2481.cpp
@@ -0,0 +1,35 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution {
+public:
+    int numberOfCuts(int n) {
+        return n & 1 ? (n == 1 ? 0 : n) : n / 2;
+    }
+};
+
+int main()
+{
+    io;
+    Solution s;
+    vv v1 = {{2, 1}, {3, 1}, {1, 4}};
+    cout << s.minNumberOfSemesters(4, v1, 2) << endl;
+    return 0;
+}
