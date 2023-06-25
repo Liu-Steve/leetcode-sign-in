ef93a54d8f8673438da3cbfa969124542c655b35
Liu-Steve <steve_liu@whu.edu.cn>
2023-06-26 00:31:25 +0800

    20230626

---
+++ 20230626-2485.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int s = n * (n + 1) / 2;
+        int k = sqrt(s);
+        return k * k == s ? k : -1;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
