1c3769c10ce86a90327ab6eee7feee92b3639cf8
Liu-Steve <steve_liu@whu.edu.cn>
2023-07-22 23:29:00 +0800

    20230722

---
+++ 20230722-860.cpp
@@ -0,0 +1,89 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+// class Solution {
+// public:
+//     bool lemonadeChange(vector<int>& bills) {
+//         int b5 = 0;
+//         int b10 = 0;
+//         int b20 = 0;
+//         for (int b : bills)
+//             if (b == 5)
+//                 b5++;
+//             else if (b == 10)
+//                 b10++;
+//             else if (b == 20)
+//                 b20++;
+//         if (b10 > b5)
+//             return false;
+//         if (b20 > b10 && 3 * b20 - b10 > b5)
+//             return false;
+//         if (b20 <= b10 && b20 > b5 - b10)
+//             return false;
+//         return true;
+//     }
+// };
+
+class Solution
+{
+public:
+    bool lemonadeChange(vector<int> &bills)
+    {
+        int b5 = 0;
+        int b10 = 0;
+        int b20 = 0;
+        for (int b : bills)
+        {
+            if (b == 5)
+                b5++;
+            else if (b == 10)
+            {
+                if (b5 == 0)
+                    return false;
+                b10++;
+                b5--;
+            }
+            else if (b == 20)
+            {
+                if (b10 == 0)
+                {
+                    if (b5 < 3)
+                        return false;
+                    b5 -= 3;
+                }
+                else
+                {
+                    if (b5 == 0)
+                        return false;
+                    b5--;
+                    b10--;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    io;
+    Solution s;
+    return 0;
+}
