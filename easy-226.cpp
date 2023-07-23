5abf41401607381ec17be765af2e714ceb092cc4
Liu-Steve <steve_liu@whu.edu.cn>
2023-07-23 23:48:27 +0800

    ict-p3

---
+++ easy-226.cpp
@@ -0,0 +1,50 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *pRoot)
+    {
+        if (pRoot == nullptr)
+            return nullptr;
+        pRoot->left = invertTree(pRoot->left);
+        pRoot->right = invertTree(pRoot->right);
+        swap(pRoot->left, pRoot->right);
+        return pRoot;
+    }
+};
+
+int main()
+{
+    io;
+    Solution s;
+    return 0;
+}
