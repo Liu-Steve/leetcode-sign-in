04673def239ae8a2f11ad73e948f34aabf9d4fe1
Liu-Steve <steve_liu@whu.edu.cn>
2023-07-29 14:41:34 +0800

    20230729

---
+++ 20230729-141.cpp
@@ -0,0 +1,49 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        while (head != nullptr)
+        {
+            if (head->val == INT32_MAX)
+                return true;
+            head->val = INT32_MAX;
+            head = head->next;
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    io;
+    Solution s;
+    return 0;
+}
