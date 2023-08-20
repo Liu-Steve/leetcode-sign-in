828a471a9efcfdeab7313def9f985aacad5882d8
Liu-Steve <steve_liu@whu.edu.cn>
2023-08-20 16:22:16 +0800

    20230820

---
+++ 20230820-2236.cpp
@@ -0,0 +1,45 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    bool checkTree(TreeNode *root)
+    {
+        return root->val == root->left->val + root->right->val;
+    }
+};
+
+int main()
+{
+    io;
+    Solution s;
+    return 0;
+}
