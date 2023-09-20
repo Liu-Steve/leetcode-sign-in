7f807be2c870703201218bc4078fd5ce72b17ec5
Liu-Steve <steve_liu@whu.edu.cn>
2023-09-20 22:08:37 +0800

    20230920

---
+++ 20230920-LCP-06.cpp
@@ -0,0 +1,38 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    int minCount(vector<int> &coins)
+    {
+        int sum = 0;
+        for (int c : coins)
+            sum += (c + 1) / 2;
+        return sum;
+    }
+};
+
+int main()
+{
+    io;
+    Solution s;
+    return 0;
+}
