694d4c015bddda2091240cd69633c4b6396e2887
Liu-Steve <steve_liu@whu.edu.cn>
2023-10-18 01:11:32 +0800

    20231018

---
+++ 20231018-2530.cpp
@@ -0,0 +1,47 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    long long maxKelements(vector<int> &nums, int k)
+    {
+        priority_queue<int> pq;
+        for (int num : nums)
+            pq.emplace(num);
+        long long ans = 0;
+        while (k--)
+        {
+            ans += pq.top();
+            int tmp = ceil(pq.top() / 3.0);
+            pq.pop();
+            pq.emplace(tmp);
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    Solution s;
+    return 0;
+}
