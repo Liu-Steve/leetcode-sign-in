1a043eb3bcf0309e8e88d3c631dc8e44514a8b2c
Liu-Steve <steve_liu@whu.edu.cn>
2023-10-23 08:22:15 +0800

    20231023

---
+++ 20231023-2678.cpp
@@ -0,0 +1,42 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    int countSeniors(vector<string> &details)
+    {
+        int ans = 0;
+        for (auto &s : details)
+        {
+            int age = (s[11] - '0') * 10 + (s[12] - '0');
+            ans += (age > 60);
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    Solution s;
+    return 0;
+}
