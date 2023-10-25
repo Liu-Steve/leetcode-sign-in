4819bb714b369b789d63c737157e64d2afb74955
Liu-Steve <steve_liu@whu.edu.cn>
2023-10-25 23:42:48 +0800

    20231025

---
+++ 20231025-2698.cpp
@@ -0,0 +1,41 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    int punishmentNumber(int n)
+    {
+        vector<int> pre = {1, 9, 10, 36, 45, 55, 82, 91, 99, 100, 235, 297, 369, 370, 379, 414, 657, 675, 703, 756, 792, 909, 918, 945, 964, 990, 991, 999, 1000};
+        int ans = 0;
+        for (int p : pre)
+            if (p <= n)
+                ans += p * p;
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    Solution s;
+    return 0;
+}
