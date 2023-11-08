ea7e0cfe9b28a2ff2586a698a17ca9a889779a2d
Liu-Steve <steve_liu@whu.edu.cn>
2023-11-08 21:21:47 +0800

    20231108

---
+++ 20231108-2609.cpp
@@ -0,0 +1,56 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    int findTheLongestBalancedSubstring(string s)
+    {
+        int ans = 0;
+        int a = 0;
+        int b = 0;
+        for (char ch : s)
+        {
+            if (ch == '0')
+            {
+                if (b != 0)
+                {
+                    a = 0;
+                    b = 0;
+                }
+                a++;
+            }
+            else
+            {
+                b++;
+                ans = max(ans, min(a, b));
+            }
+        }
+        return ans * 2;
+    }
+};
+
+int main()
+{
+    io;
+    Solution s;
+    return 0;
+}
