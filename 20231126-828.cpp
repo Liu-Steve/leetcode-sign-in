3c3ca640e62cfd2574439b32e435ce75d2e9544f
Liu-Steve <steve_liu@whu.edu.cn>
2023-11-26 23:18:24 +0800

    20231126

---
+++ 20231126-828.cpp
@@ -0,0 +1,50 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    int uniqueLetterString(string &s)
+    {
+        int n = s.size();
+        vector<int> ans(n);
+        vector<int> lst(26, -1);
+        for (int i = 0; i < n; ++i)
+        {
+            ans[i] = i - lst[s[i] - 'A'];
+            lst[s[i] - 'A'] = i;
+        }
+        fill(lst.begin(), lst.end(), n);
+        for (int i = n - 1; i >= 0; --i)
+        {
+            ans[i] *= lst[s[i] - 'A'] - i;
+            lst[s[i] - 'A'] = i;
+        }
+        return accumulate(ans.begin(), ans.end(), 0);
+    }
+};
+
+int main()
+{
+    io;
+    Solution s;
+    return 0;
+}
