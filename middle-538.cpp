73f4a9246a314c52970bd23393192962e7a2b642
Liu-Steve <steve_liu@whu.edu.cn>
2023-12-04 16:16:59 +0800

    middle 538

---
+++ middle-538.cpp
@@ -0,0 +1,56 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+private:
+    int dfs(TreeNode *root, int sum)
+    {
+        if (root == nullptr)
+            return sum;
+        root->val += dfs(root->right, sum);
+        return dfs(root->left, root->val);
+    }
+
+public:
+    TreeNode *convertBST(TreeNode *root)
+    {
+        dfs(root, 0);
+        return root;
+    }
+};
+
+int main()
+{
+    io;
+    Solution s;
+    return 0;
+}
