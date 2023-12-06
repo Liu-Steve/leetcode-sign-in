9117b610c70802cc2ecd9dbc4f3f87ec5b2e136b
Liu-Steve <steve_liu@whu.edu.cn>
2023-12-06 23:46:18 +0800

    20231106

---
+++ 20231106-318.cpp
@@ -0,0 +1,46 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    int maxProduct(vector<string> &words)
+    {
+        int n = words.size();
+        vector<int> mask(n);
+        for (int i = 0; i < n; ++i)
+            for (char c : words[i])
+                mask[i] |= (1 << (c - 'a'));
+        int ans = 0;
+        for (int i = 0; i < n; ++i)
+            for (int j = i + 1; j < n; ++j)
+                if ((mask[i] & mask[j]) == 0)
+                    ans = max(ans, (int)words[i].size() * (int)words[j].size());
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    Solution s;
+    return 0;
+}
