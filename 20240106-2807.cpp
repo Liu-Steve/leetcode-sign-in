964ac64c14d9792c2be98d27428f237c840b54de
Liu-Steve <steve_liu@whu.edu.cn>
2024-01-08 21:00:39 +0800

    20240106

---
+++ 20240106-2807.cpp
@@ -0,0 +1,54 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *insertGreatestCommonDivisors(ListNode *head)
+    {
+        ListNode *p1 = head;
+        ListNode *p2 = head->next;
+        while (p2 != nullptr)
+        {
+            p1->next = new ListNode(gcd(p1->val, p2->val), p2);
+            p1 = p2;
+            p2 = p2->next;
+        }
+        return head;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
