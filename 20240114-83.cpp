28d00a5b1d11f81a4831e47f6090d3902badab4c
Liu-Steve <steve_liu@whu.edu.cn>
2024-01-15 11:49:22 +0800

    20240114

---
+++ 20240114-83.cpp
@@ -0,0 +1,54 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *deleteDuplicates(ListNode *head)
+    {
+        ListNode *p = head;
+        if (p == nullptr)
+            return nullptr;
+        while (p->next != nullptr)
+            if (p->val == p->next->val)
+                p->next = p->next->next;
+            else
+                p = p->next;
+        return head;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
