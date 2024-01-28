033aba29d3113e690c35e64410a2d9688a5ee8ec
Liu-Steve <steve_liu@whu.edu.cn>
2024-01-28 21:45:58 +0800

    20240128

---
+++ 20240128-365.cpp
@@ -0,0 +1,37 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    bool canMeasureWater(int x, int y, int z)
+    {
+        return x + y >= z && z % gcd(x, y) == 0;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
