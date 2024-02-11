924c63f70d9324b75f128c43b448678e4a80dcd2
Liu-Steve <steve_liu@whu.edu.cn>
2024-02-11 12:30:43 +0800

    20240211

---
+++ 20240211-144.cpp
@@ -0,0 +1,57 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    vector<int> preorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+        function<void(TreeNode *)> dfs = [&](TreeNode *root)
+        {
+            if (root == nullptr)
+                return;
+            ans.push_back(root->val);
+            dfs(root->left);
+            dfs(root->right);
+        };
+        dfs(root);
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
