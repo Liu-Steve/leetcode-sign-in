55c98ddd82f4a8dd4ffa5cbd91333ad31dadd417
Liu-Steve <steve_liu@whu.edu.cn>
2024-03-07 23:40:32 +0800

    20240307

---
+++ 20240307-2575.cpp
@@ -0,0 +1,77 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<ll, ll> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    vector<int> divisibilityArray(string word, int m)
+    {
+        long long num = 0;
+        int n = word.size();
+        vector<int> ans(n);
+        for (int i = 0; i < n; ++i)
+        {
+            num *= 10;
+            num += word[i] - '0';
+            num %= m;
+            ans[i] = (num == 0);
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
