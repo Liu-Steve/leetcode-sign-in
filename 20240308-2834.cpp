ad5a5cd1773a4f6c27a3d909b877a2303df5da2b
Liu-Steve <steve_liu@whu.edu.cn>
2024-03-08 23:37:49 +0800

    20240308

---
+++ 20240308-2834.cpp
@@ -0,0 +1,70 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<ll, ll> pr;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    int minimumPossibleSum(int n, int target)
+    {
+        int m = target / 2;
+        if (n <= m)
+            return (ll)(1 + n) * n / 2 % MOD;
+        return ((ll)(1 + m) * m / 2 + ((ll)target + target + (n - m) - 1) * (n - m) / 2) % MOD;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
