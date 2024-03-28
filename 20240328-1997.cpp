69468fd3de76568b3dc0fb764f8f44d11fcf9a5f
Liu-Steve <steve_liu@whu.edu.cn>
2024-03-28 15:04:56 +0800

    20240328

---
+++ 20240328-1997.cpp
@@ -0,0 +1,73 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    int firstDayBeenInAllRooms(vector<int> &nextVisit)
+    {
+        int n = nextVisit.size();
+        vector<long long> s(n, 0);
+        for (int i = 0; i < n - 1; ++i)
+            s[i + 1] = (s[i] * 2 - s[nextVisit[i]] + 2 + MOD) % MOD;
+        return s[n - 1];
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
