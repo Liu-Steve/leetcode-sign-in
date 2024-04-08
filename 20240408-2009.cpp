3dabaf2d3ba25c9e2db14c29c82bc0ba4755a675
Liu-Steve <steve_liu@whu.edu.cn>
2024-04-09 03:02:31 +0800

    20240408

---
+++ 20240408-2009.cpp
@@ -0,0 +1,83 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    int minOperations(vector<int> &nums)
+    {
+        int n = nums.size();
+        unordered_set<int> cnt(nums.begin(), nums.end());
+        vector<int> sortedUniqueNums(cnt.begin(), cnt.end());
+        sort(sortedUniqueNums.begin(), sortedUniqueNums.end());
+        int res = n, j = 0;
+        for (int i = 0; i < sortedUniqueNums.size(); i++)
+        {
+            int right = sortedUniqueNums[i] + n - 1;
+            while (j < sortedUniqueNums.size() && sortedUniqueNums[j] <= right)
+            {
+                res = min(res, n - (j - i + 1));
+                j++;
+            }
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
