db135b11486d31fab789f9981044133b7af8eb5a
Liu-Steve <steve_liu@whu.edu.cn>
2024-05-08 17:04:30 +0800

    20240508

---
+++ 20240508-2079.cpp
@@ -0,0 +1,83 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    int wateringPlants(vector<int> &plants, int capacity)
+    {
+        int ans = 0;
+        int n = plants.size();
+        int r = capacity;
+        for (int i = 0; i < n; ++i)
+        {
+            if (r >= plants[i])
+            {
+                r -= plants[i];
+                ans++;
+                continue;
+            }
+            ans += i * 2 + 1;
+            r = capacity - plants[i];
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
