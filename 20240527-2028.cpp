fb79d8b198d280db396f702d486c82b7ae7e39dc
Liu-Steve <steve_liu@whu.edu.cn>
2024-05-27 15:16:09 +0800

    20240527

---
+++ 20240527-2028.cpp
@@ -0,0 +1,87 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    vector<int> missingRolls(vector<int> &rolls, int mean, int n)
+    {
+        int m = rolls.size();
+        int sum = mean * (m + n);
+        for (int r : rolls)
+            sum -= r;
+        if (sum < n || sum > 6 * n)
+            return vector<int>{};
+        sum -= n;
+        vector<int> ans(n, 1);
+        for (int i = 0; i < n; ++i)
+        {
+            if (sum <= 5)
+            {
+                ans[i] += sum;
+                break;
+            }
+            ans[i] = 6;
+            sum -= 5;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
