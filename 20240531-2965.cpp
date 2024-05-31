e5fe0370681af96dfb71a8d650ba8133d901fcf0
Liu-Steve <steve_liu@whu.edu.cn>
2024-05-31 15:50:21 +0800

    20240531

---
+++ 20240531-2965.cpp
@@ -0,0 +1,82 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>> &grid)
+    {
+        int n = grid.size();
+        vector<int> cnt(n * n + 1);
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                cnt[grid[i][j]]++;
+        vector<int> ans(2);
+        for (int i = 1; i <= n * n; ++i)
+        {
+            if (cnt[i] == 2)
+                ans[0] = i;
+            else if (cnt[i] == 0)
+                ans[1] = i;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
