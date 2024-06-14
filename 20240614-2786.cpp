884c53c7c4a574144e9f207c52e498cee70c6dcf
Liu-Steve <steve_liu@whu.edu.cn>
2024-06-14 23:57:32 +0800

    20240614

---
+++ 20240614-2786.cpp
@@ -0,0 +1,77 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    long long maxScore(vector<int> &nums, int x)
+    {
+        vector<long long> dp(2, -0x7f7f7f7f);
+        dp[nums[0] % 2] = nums[0];
+        for (int i = 1; i < nums.size(); ++i)
+        {
+            long long num = nums[i];
+            int idx = num % 2;
+            dp[idx] = max(dp[idx] + num, dp[!idx] + num - x);
+        }
+        return max(dp[0], dp[1]);
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
