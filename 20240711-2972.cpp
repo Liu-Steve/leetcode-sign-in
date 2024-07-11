3520d5348a89499627109f600998acf356fd2bec
Liu-Steve <steve_liu@whu.edu.cn>
2024-07-11 15:18:39 +0800

    20240711

---
+++ 20240711-2972.cpp
@@ -0,0 +1,90 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    long long incremovableSubarrayCount(vector<int> &nums)
+    {
+        int n = nums.size();
+        int l = 0;
+        int r = n - 1;
+        int l_lst = 0;
+        int r_lst = INT32_MAX;
+        while (l <= r && nums[l] > l_lst)
+        {
+            l_lst = nums[l];
+            l++;
+        }
+        long long cnt = 0;
+        while (r >= 0)
+        {
+            while (l > r || l > 0 && nums[l - 1] >= r_lst)
+                l--;
+            cnt += l + 1;
+            if (nums[r] >= r_lst)
+                break;
+            r_lst = nums[r];
+            r--;
+        }
+        return cnt;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
