b4f0118efcd3bb810f6377f66d226e11dd2ead63
Liu-Steve <steve_liu@whu.edu.cn>
2024-07-29 15:33:44 +0800

    20240727

---
+++ 20240727-3106.cpp
@@ -0,0 +1,81 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    string getSmallestString(string s, int k)
+    {
+        for (char &ch : s)
+        {
+            int step = min(ch - 'a', 26 - ch + 'a');
+            if (step <= k)
+            {
+                ch = 'a';
+                k -= step;
+                continue;
+            }
+            ch -= k;
+            break;
+        }
+        return s;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
