1994ab1ef23b6cd26d5b25aead13dc83f05c97c4
Liu-Steve <steve_liu@whu.edu.cn>
2024-08-14 18:00:19 +0800

    20240814

---
+++ 20240814-3152.cpp
@@ -0,0 +1,79 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<int, int> memory;
+
+class Solution
+{
+public:
+    vector<bool> isArraySpecial(vector<int> &nums, vector<vector<int>> &queries)
+    {
+        int n = nums.size();
+        vector<int> lst(n);
+        for (int i = 1; i < n; ++i)
+            if (((nums[i] ^ nums[i - 1]) & 1) == 0)
+                lst[i] = i;
+            else
+                lst[i] = lst[i - 1];
+        vector<bool> ans;
+        for (auto &q : queries)
+            ans.push_back(lst[q[1]] <= q[0]);
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
