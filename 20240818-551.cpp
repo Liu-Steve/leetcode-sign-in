dc1089c2b4c97b79f35e00227c7b8ad46bf9d8b0
Liu-Steve <steve_liu@whu.edu.cn>
2024-08-18 20:19:10 +0800

    20240818

---
+++ 20240818-551.cpp
@@ -0,0 +1,82 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+class Solution
+{
+public:
+    bool checkRecord(string s)
+    {
+        int cnt_A = 0;
+        int cnt_L = 0;
+        for (char ch : s)
+        {
+            if (ch == 'A')
+                cnt_A++;
+            if (ch == 'L')
+                cnt_L++;
+            else
+                cnt_L = 0;
+            if (cnt_A == 2 || cnt_L == 3)
+                return false;
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
