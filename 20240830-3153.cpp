5dc875b677453207cb2144967fbb70156ee1ce4e
Liu-Steve <steve_liu@whu.edu.cn>
2024-08-30 17:34:10 +0800

    20240830

---
+++ 20240830-3153.cpp
@@ -0,0 +1,82 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+class Solution
+{
+public:
+    long long sumDigitDifferences(vector<int> &nums)
+    {
+        long long ans = 0;
+        for (int i = 0; i < 9; ++i)
+        {
+            vector<long long> cnt(10);
+            for (int &num : nums)
+            {
+                cnt[num % 10]++;
+                num /= 10;
+            }
+            for (int i = 0; i < 10; ++i)
+                for (int j = 0; j < i; ++j)
+                    ans += cnt[i] * cnt[j];
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
