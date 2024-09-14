a7a5ac4e626acef374b17b05af00ce1614d1c210
Liu-Steve <steve_liu@whu.edu.cn>
2024-09-14 16:37:35 +0800

    20240913

---
+++ 20240913-2398.cpp
@@ -0,0 +1,98 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+class Solution
+{
+public:
+    int maximumRobots(vector<int> &chargeTimes, vector<int> &runningCosts, long long budget)
+    {
+        int n = chargeTimes.size();
+        int l = 0;
+        int r = 0;
+        long long sum = 0;
+        int ans = 0;
+        deque<int> q;
+        for (; r < n; ++r)
+        {
+            while (!q.empty() && chargeTimes[q.back()] <= chargeTimes[r])
+                q.pop_back();
+            q.push_back(r);
+            sum += runningCosts[r];
+            while (!q.empty() && chargeTimes[q.front()] + (r - l + 1) * sum > budget)
+            {
+                if (q.front() == l)
+                    q.pop_front();
+                sum -= runningCosts[l++];
+            }
+            ans = max(ans, r - l + 1);
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
