81282af04bf48bb15a8ff8d55a57128611663525
Liu-Steve <steve_liu@whu.edu.cn>
2024-09-25 19:28:03 +0800

    20240925

---
+++ 20240925-2306.cpp
@@ -0,0 +1,91 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+class Solution
+{
+public:
+    long long distinctNames(vector<string> &ideas)
+    {
+        long long ans = 0;
+        unordered_set<string> s[26];
+        for (string &i : ideas)
+            s[i[0] - 'a'].insert(i.substr(1));
+        for (int i = 0; i < 26; ++i)
+            for (int j = i + 1; j < 26; ++j)
+            {
+                long long cnt = 0;
+                for (auto &str : s[i])
+                    if (s[j].count(str))
+                        cnt++;
+                ans += 2 * (s[i].size() - cnt) * (s[j].size() - cnt);
+            }
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
