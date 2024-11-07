4047708a289dba3212863d3fc04a4d9fcdc71cbe
Liu-Steve <steve_liu@whu.edu.cn>
2024-11-07 13:33:59 +0800

    20241107

---
+++ 20241107-3255.cpp
@@ -0,0 +1,89 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+class Solution
+{
+public:
+    vector<int> resultsArray(vector<int> &nums, int k)
+    {
+        int cnt = 0;
+        int lst = -1;
+        int n = nums.size();
+        vector<int> ans(n - k + 1, -1);
+        for (int i = 0; i < n; ++i)
+        {
+            cnt = (nums[i] != lst + 1) ? 1 : (cnt + 1);
+            lst = nums[i];
+            if (cnt >= k)
+                ans[i - k + 1] = nums[i];
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
