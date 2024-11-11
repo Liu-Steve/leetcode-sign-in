194074ab8f9ba1773715f5b3df85fe06e7ac0903
Liu-Steve <steve_liu@whu.edu.cn>
2024-11-11 13:48:42 +0800

    20241111

---
+++ 20241111-1547.cpp
@@ -0,0 +1,95 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+class Solution
+{
+public:
+    int minCost(int n, vector<int> &cuts)
+    {
+        cuts.push_back(0);
+        cuts.push_back(n);
+        sort(cuts.begin(), cuts.end());
+        int m = cuts.size();
+        vector<vector<int>> dp(m, vector<int>(m));
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = i - 2; j >= 0; --j)
+            {
+                int cost = INT32_MAX;
+                for (int k = j + 1; k <= i - 1; ++k)
+                {
+                    cost = min(cost, dp[j][k] + dp[k][i]);
+                }
+                dp[j][i] = cost + cuts[i] - cuts[j];
+            }
+        }
+        return dp[0][m - 1];
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
