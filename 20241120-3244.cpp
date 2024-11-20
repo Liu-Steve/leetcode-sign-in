7cb21011f8f02160f8a3c8abb1fd83ff4d17ccfe
Liu-Steve <steve_liu@whu.edu.cn>
2024-11-20 23:41:43 +0800

    20241120

---
+++ 20241120-3244.cpp
@@ -0,0 +1,95 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+class Solution
+{
+public:
+    vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>> &queries)
+    {
+        vector<int> roads(n);
+        iota(roads.begin(), roads.end(), 1);
+        vector<int> res;
+        int dist = n - 1;
+        for (auto &q : queries)
+        {
+            int k = roads[q[0]];
+            roads[q[0]] = q[1];
+            while (k != -1 && k < q[1])
+            {
+                int t = roads[k];
+                roads[k] = -1;
+                k = t;
+                dist--;
+            }
+            res.push_back(dist);
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
