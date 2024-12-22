eafd29edba3a2463a594f9a8813e49e777c90092
Liu-Steve <steve_liu@whu.edu.cn>
2024-12-22 18:28:26 +0800

    20241222

---
+++ 20241222-1387.cpp
@@ -0,0 +1,96 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+class Solution
+{
+private:
+    int cnt(int num){
+        int ans = 0;
+        while (num != 1)
+        {
+            if (num & 1)
+                num = num * 3 + 1;
+            else
+                num /= 2;
+            ans++;
+        }
+        return ans;
+    }
+
+public:
+    int getKth(int lo, int hi, int k)
+    {
+        vector<pair<int, int>> m;
+        for (int i = lo; i <= hi; ++i)
+            m.emplace_back(cnt(i), i);
+        sort(m.begin(), m.end());
+        return m[k - 1].second; 
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
