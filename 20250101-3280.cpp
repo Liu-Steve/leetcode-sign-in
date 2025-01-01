0d6c8fd7293e876ce9b0db440f3b6acaa858f73b
Liu-Steve <steve_liu@whu.edu.cn>
2025-01-01 18:43:56 +0800

    20250101

---
+++ 20250101-3280.cpp
@@ -0,0 +1,98 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+class Solution
+{
+public:
+    string convertDateToBinary(string date)
+    {
+        int year = (date[0] - '0') * 1000 + (date[1] - '0') * 100 + (date[2] - '0') * 10 + (date[3] - '0');
+        int month = (date[5] - '0') * 10 + (date[6] - '0');
+        int day = (date[8] - '0') * 10 + (date[9] - '0');
+        stringstream ss;
+        auto binary = [&](int num)
+        {
+            vector<int> v;
+            while (num)
+            {
+                v.push_back(num & 1);
+                num >>= 1;
+            }
+            for (auto i = v.rbegin(); i != v.rend(); ++i)
+                ss << *i;
+        };
+        binary(year);
+        ss << '-';
+        binary(month);
+        ss << '-';
+        binary(day);
+        return ss.str();
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
