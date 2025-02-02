72732c6be4eccf1eb72874a874be407940f6d739
Liu-Steve <steve_liu@whu.edu.cn>
2025-02-02 21:06:10 +0800

    20250131

---
+++ 20250131-541.cpp
@@ -0,0 +1,94 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+class Solution
+{
+public:
+    string reverseStr(string s, int k)
+    {
+        auto rev = [&](int l, int r)
+        {
+            while (l < r)
+            {
+                swap(s[l], s[r]);
+                l++;
+                r--;
+            }
+        };
+        for (int i = 0; i < s.size(); i += 2 * k)
+        {
+            if (i + k > s.size())
+                rev(i, s.size() - 1);
+            else
+                rev(i, i + k - 1);
+        }
+        return s;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
