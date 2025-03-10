bae976dbfcd81300df232c75bd5a1ef84ca52c7c
Liu-Steve <steve_liu@whu.edu.cn>
2025-03-10 13:23:05 +0800

    20250310

---
+++ 20250310-2269.cpp
@@ -0,0 +1,99 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+class Solution
+{
+public:
+    int divisorSubstrings(int num, int k)
+    {
+        long long l = 1;
+        long long r = 1;
+        for (int i = 0; i < k; ++i)
+            l *= 10;
+        int ans = 0;
+        while (true)
+        {
+            int mod = (num % l) / r;
+            if (mod == 0)
+            {
+                l *= 10;
+                r *= 10;
+                continue;
+            }
+            if (num % mod == 0)
+                ans++;
+            if (l > num)
+                break;
+            l *= 10;
+            r *= 10;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
