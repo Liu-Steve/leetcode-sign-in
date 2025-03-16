04fca94d8b1310ef674185a4baac3988519089e8
Liu-Steve <steve_liu@whu.edu.cn>
2025-03-16 22:04:35 +0800

    20250316

---
+++ 20250316-2272.cpp
@@ -0,0 +1,94 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+class Solution
+{
+public:
+    int largestVariance(string s)
+    {
+        int ans = 0;
+        vector f0(26, vector<int>(26, 0));
+        vector f1(26, vector<int>(26, INT32_MIN));
+        for (char ch : s)
+        {
+            ch -= 'a';
+            for (int i = 0; i < 26; ++i)
+            {
+                if (ch == i)
+                    continue;
+                f0[ch][i] = max(f0[ch][i], 0) + 1;
+                f1[ch][i]++;
+                f0[i][ch] = f1[i][ch] = max(f0[i][ch], 0) - 1;
+                ans = max(ans, max(f1[ch][i], f1[i][ch]));
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
