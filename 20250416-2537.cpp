a44be287290f1b2f54f9b4a133d3ac81f5c54dc3
Liu-Steve <steve_liu@whu.edu.cn>
2025-04-16 09:58:51 +0800

    20250416

---
+++ 20250416-2537.cpp
@@ -0,0 +1,96 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+#define MOD 1000000007LL
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+class Solution
+{
+public:
+    long long countGood(vector<int> &nums, int k)
+    {
+        long long ans = 0;
+        int l = 0;
+        int r = 0;
+        int n = nums.size();
+        long long cnt = 0;
+        unordered_map<int, int> mp;
+        for (; r < n; ++r)
+        {
+            mp[nums[r]]++;
+            cnt += mp[nums[r]] - 1;
+            while (cnt >= k)
+            {
+                mp[nums[l]]--;
+                cnt -= mp[nums[l]];
+                l++;
+            }
+            ans += l;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
