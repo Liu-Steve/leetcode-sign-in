518d6c52b89f1f9da182ce52864c689571ad6488
Liu-Steve <steve_liu@whu.edu.cn>
2025-06-24 16:31:02 +0800

    20250624

---
+++ 20250624-2200.cpp
@@ -0,0 +1,95 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+const long long MOD = 1000000007LL;
+
+class Solution
+{
+public:
+    vector<int> findKDistantIndices(vector<int> &nums, int key, int k)
+    {
+        int l = -1010;
+        int r = -1010;
+        int n = nums.size();
+        vector<int> ans;
+        for (int i = 0; i < n; ++i)
+        {
+            if (i > r)
+            {
+                l = r;
+                for (r = i; r < n && nums[r] != key; ++r)
+                    ;
+                if (r >= n)
+                    r = n + 1010;
+            }
+            if (i - l <= k || r - i <= k)
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
