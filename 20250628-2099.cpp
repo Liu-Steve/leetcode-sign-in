45bc051ef7942bbc15f1f015749ac4d43e8a49c6
Liu-Steve <steve_liu@whu.edu.cn>
2025-06-28 16:05:27 +0800

    20250628

---
+++ 20250628-2099.cpp
@@ -0,0 +1,87 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+const long long MOD = 1000000007LL;
+
+class Solution
+{
+public:
+    vector<int> maxSubsequence(vector<int> &nums, int k)
+    {
+        vector<int> idx;
+        for (int i = 0; i < nums.size(); ++i)
+            idx.push_back(i);
+        sort(idx.begin(), idx.end(), [&](int i1, int i2)
+             { return nums[i1] > nums[i2]; });
+        sort(idx.begin(), idx.begin() + k);
+        vector<int> ans;
+        for (int i = 0; i < k; ++i)
+            ans.push_back(nums[idx[i]]);
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
