bc6ba7399db68923332cf3596ed7dd9f499526b9
Liu-Steve <steve_liu@whu.edu.cn>
2025-07-07 15:24:18 +0800

    20250707

---
+++ 20250707-1353.cpp
@@ -0,0 +1,96 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+const long long MOD = 1000000007LL;
+
+class Solution
+{
+public:
+    int maxEvents(vector<vector<int>> &events)
+    {
+        sort(events.begin(), events.end());
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int p = 0;
+        int ans = 0;
+        for (int i = 1; i <= 100000; ++i)
+        {
+            while (p < events.size() && events[p][0] <= i)
+            {
+                pq.push(events[p][1]);
+                p++;
+            }
+            while (!pq.empty() && pq.top() < i)
+                pq.pop();
+            if (pq.empty())
+                continue;
+            pq.pop();
+            ans++;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
