6a666d26c44b9107559edbaadc05da0dc816edba
Liu-Steve <steve_liu@whu.edu.cn>
2025-08-10 17:22:49 +0800

    20250810

---
+++ 20250810-869.cpp
@@ -0,0 +1,95 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+const long long MOD = 1000000007LL;
+
+class Solution
+{
+public:
+    bool reorderedPowerOf2(int n)
+    {
+        auto reorder = [&](int num) -> string
+        {
+            vector<int> digit;
+            while (num)
+            {
+                digit.push_back(num % 10);
+                num /= 10;
+            }
+            sort(digit.begin(), digit.end());
+            stringstream ss;
+            for (int d : digit)
+                ss << d;
+            return ss.str();
+        };
+        unordered_set<string> s;
+        for (int i = 1; i > 0; i <<= 1)
+            s.insert(reorder(i));
+        return s.count(reorder(n));
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
