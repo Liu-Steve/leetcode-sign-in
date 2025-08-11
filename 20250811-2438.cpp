06d4ed952f42eec9efbbce2e1eb1bdbd1dc5715b
Liu-Steve <steve_liu@whu.edu.cn>
2025-08-11 16:44:39 +0800

    20250811

---
+++ 20250811-2438.cpp
@@ -0,0 +1,99 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+const long long MOD = 1000000007LL;
+
+long long qpow(long long a, long long b)
+{
+    long long ans = 1;
+    while (b)
+    {
+        if (b & 1)
+            ans = (ans * a) % MOD;
+        b >>= 1;
+        a = (a * a) % MOD;
+    }
+    return ans % MOD;
+}
+
+class Solution
+{
+public:
+    vector<int> productQueries(int n, vector<vector<int>> &queries)
+    {
+        vector<int> prefix;
+        prefix.push_back(0);
+        for (int i = 0; i < 32; ++i)
+            if (n & (1 << i))
+                prefix.push_back(prefix.back() + i);
+        vector<int> ans;
+        for (auto &q : queries)
+            ans.push_back(qpow(2, prefix[q[1] + 1] - prefix[q[0]]));
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
