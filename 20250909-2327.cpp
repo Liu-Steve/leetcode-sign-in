df4a48f403d1606a80eb842ec4d6ccab4062982c
Liu-Steve <steveliu123@hotmail.com>
2025-09-09 16:28:39 +0800

    20250909

---
+++ 20250909-2327.cpp
@@ -0,0 +1,90 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution
+{
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget)
+    {
+        vector<long long> q(forget);
+        int p_forget = 0;
+        int p_delay = forget - delay;
+        long long sum = 0;
+        q.back() = 1;
+        for (int i = 2; i <= n; ++i)
+        {
+            sum = (sum - q[p_forget] + q[p_delay] + MOD) % MOD;
+            q[p_forget] = sum;
+            p_forget = (p_forget + 1) % forget;
+            p_delay = (p_delay + 1) % forget;
+        }
+        return accumulate(q.begin(), q.end(), 0ll) % MOD;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
