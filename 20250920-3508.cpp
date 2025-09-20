4fa455f2fb48b4de7432a8fddbe3d2fa7959bfa3
Liu-Steve <steveliu123@hotmail.com>
2025-09-20 21:27:32 +0800

    20250920

---
+++ 20250920-3508.cpp
@@ -0,0 +1,120 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Router
+{
+public:
+    int memLimit = 0;
+    int length = 0;
+    set<tuple<int, int, int>> isExist;
+    unordered_map<int, deque<int>> sameDestQue;
+    deque<tuple<int, int, int>> que;
+
+    Router(int memoryLimit) { memLimit = memoryLimit; }
+
+    bool addPacket(int source, int destination, int timestamp)
+    {
+        tuple<int, int, int> packet =
+            make_tuple(source, destination, timestamp);
+        if (isExist.contains(packet))
+            return false;
+        if (length == memLimit)
+            forwardPacket();
+        length++;
+        que.push_back(packet);
+        sameDestQue[destination].push_back(timestamp);
+        isExist.insert(packet);
+        return true;
+    }
+
+    vector<int> forwardPacket()
+    {
+        vector<int> data;
+        if (!que.empty())
+        {
+            tuple<int, int, int> packet = que.front();
+            que.pop_front();
+            data = vector<int>{get<0>(packet), get<1>(packet), get<2>(packet)};
+            isExist.erase(packet);
+            sameDestQue[data[1]].pop_front();
+            length--;
+        }
+        return data;
+    }
+
+    int getCount(int destination, int startTime, int endTime)
+    {
+        auto pos1 = lower_bound(sameDestQue[destination].begin(),
+                                sameDestQue[destination].end(), startTime);
+        auto pos2 = upper_bound(sameDestQue[destination].begin(),
+                                sameDestQue[destination].end(), endTime);
+        return pos2 - pos1;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
