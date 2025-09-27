0849ba330ebb177f6a482f31d88d54c1c870278f
Liu-Steve <steveliu123@hotmail.com>
2025-09-27 22:19:49 +0800

    20250927

---
+++ 20250927-812.cpp
@@ -0,0 +1,89 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution
+{
+public:
+    double largestTriangleArea(vector<vector<int>> &points)
+    {
+        int n = points.size();
+        double ans = 0;
+        for (int i = 0; i < n; ++i)
+            for (int j = i + 1; j < n; ++j)
+                for (int k = j + 1; k < n; ++k)
+                    ans = max(ans, 0.5 * abs((points[i][0] * points[j][1] +
+                                              points[j][0] * points[k][1] +
+                                              points[k][0] * points[i][1]) -
+                                             (points[i][0] * points[k][1] +
+                                              points[k][0] * points[j][1] +
+                                              points[j][0] * points[i][1])));
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
