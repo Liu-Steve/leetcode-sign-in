ccd4c7c9eb0ba6d84ad88222af8cec7cb0cb3267
Liu-Steve <steveliu123@hotmail.com>
2025-11-03 15:21:45 +0800

    20251103

---
+++ 20251103-1578.cpp
@@ -0,0 +1,97 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution
+{
+public:
+    int minCost(string colors, vector<int> &neededTime)
+    {
+        int ans = 0;
+        int n = colors.size();
+        char lst = ' ';
+        int sum = 0;
+        int maximum = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            if (colors[i] == lst)
+            {
+                sum += neededTime[i];
+                maximum = max(maximum, neededTime[i]);
+                continue;
+            }
+            ans += sum - maximum;
+            lst = colors[i];
+            sum = neededTime[i];
+            maximum = neededTime[i];
+        }
+        ans += sum - maximum;
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
