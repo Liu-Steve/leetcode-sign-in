5f6104980b47f82f918fd7ca8075233b30b9de74
Liu-Steve <steveliu123@hotmail.com>
2025-12-14 17:37:50 +0800

    20251214

---
+++ 20251214-2147.cpp
@@ -0,0 +1,104 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution
+{
+public:
+    int numberOfWays(string corridor)
+    {
+        bool seat = false;
+        int plant = 0;
+        bool fnd = false;
+        long long ans = 1;
+        for (char ch : corridor)
+        {
+            if (ch == 'S')
+            {
+                fnd = true;
+                seat ^= 1;
+                if (!seat)
+                {
+                    ans *= plant + 1;
+                    plant = 0;
+                    ans %= MOD;
+                }
+            }
+            else
+            {
+                if (seat || !fnd)
+                    continue;
+                plant++;
+            }
+        }
+        if (seat || !fnd)
+            return 0;
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
