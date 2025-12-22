834db1916af97e6e346f0921fa43e9b55e0b7aa5
Liu-Steve <steveliu123@hotmail.com>
2025-12-22 17:12:41 +0800

    20251222

---
+++ 20251222-960.cpp
@@ -0,0 +1,97 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution
+{
+public:
+    int minDeletionSize(vector<string> &strs)
+    {
+        int n = strs.size();
+        int m = strs[0].size();
+        vector<int> dp(m, 1);
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                bool valid = true;
+                for (int k = 0; k < n; ++k)
+                    if (strs[k][i] < strs[k][j])
+                    {
+                        valid = false;
+                        break;
+                    }
+                if (!valid)
+                    continue;
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        return m - *max_element(dp.begin(), dp.end());
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
