ac067ef6e69f0a921f564e31dfe1f50e89b796e7
Liu-Steve <steveliu123@hotmail.com>
2025-12-26 13:46:19 +0800

    20251226

---
+++ 20251226-2483.cpp
@@ -0,0 +1,96 @@
+#include "bits/stdc++.h"
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution
+{
+public:
+    int bestClosingTime(string customers)
+    {
+        int sumY = 0;
+        int sumN = 0;
+        for (char ch : customers)
+            ch == 'Y' ? sumY++ : sumN++;
+        int cntY = 0;
+        int cntN = 0;
+        int ans = 0;
+        int cost = sumY;
+        for (int i = 0; i < customers.size(); ++i)
+        {
+            customers[i] == 'Y' ? cntY++ : cntN++;
+            int newCost = cntN + sumY - cntY;
+            if (newCost < cost)
+            {
+                cost = newCost;
+                ans = i + 1;
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    io;
+    return 0;
+}
