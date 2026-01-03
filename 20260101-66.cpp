32373ce56997591dd2290bfc3a31f77e726c09b6
Liu-Steve <steveliu123@hotmail.com>
2026-01-03 22:39:04 +0800

    20260101

---
+++ 20260101-66.cpp
@@ -0,0 +1,84 @@
+#include "bits/stdc++.h"
+#define io                                                                     \
+  ios::sync_with_stdio(false);                                                 \
+  cin.tie(0);                                                                  \
+  cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+  int val;
+  vector<Node *> children;
+
+  Node() {}
+
+  Node(int _val) { val = _val; }
+
+  Node(int _val, vector<Node *> _children) {
+    val = _val;
+    children = _children;
+  }
+};
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+  vector<int> plusOne(vector<int> &digits) {
+    int n = digits.size();
+    bool carry = true;
+    for (int i = n - 1; i >= 0; --i) {
+      if (!carry)
+        return digits;
+      digits[i]++;
+      carry = false;
+      if (digits[i] == 10) {
+        digits[i] = 0;
+        carry = true;
+      }
+    }
+    if (carry) {
+      digits.insert(digits.begin(), 1);
+    }
+    return digits;
+  }
+};
+
+int main() {
+  io;
+  return 0;
+}
