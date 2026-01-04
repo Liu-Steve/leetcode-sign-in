5fa5feb9c05dcb2c72e08ded57158805dd9607c4
Liu-Steve <steveliu123@hotmail.com>
2026-01-04 21:29:05 +0800

    20260104

---
+++ 20260104-1390.cpp
@@ -0,0 +1,97 @@
+#include "bits/stdc++.h"
+#define io                                                                     \
+  ios::sync_with_stdio(false);                                                 \
+  cin.tie(0);                                                                  \
+  cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+  int val;
+  vector<Node *> children;
+
+  Node() {}
+
+  Node(int _val) { val = _val; }
+
+  Node(int _val, vector<Node *> _children) {
+    val = _val;
+    children = _children;
+  }
+};
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+  int sumFourDivisors(vector<int> &nums) {
+    auto valid = [&](int num) {
+      int ans = num + 1;
+      bool find = false;
+      int p = 2;
+      while (p * p <= num) {
+        if (p * p == num) {
+          return 0;
+        }
+        if (num % p != 0) {
+          p++;
+          continue;
+        }
+        if (find) {
+          return 0;
+        }
+        find = true;
+        ans += p + num / p;
+        p++;
+      }
+      if (!find) {
+        return 0;
+      }
+      return ans;
+    };
+    int ans = 0;
+    for (int num : nums) {
+      ans += valid(num);
+    }
+    return ans;
+  }
+};
+
+int main() {
+  io;
+  return 0;
+}
