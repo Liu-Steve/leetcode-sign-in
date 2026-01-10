0ea7fcc64cb1f4401f144003382cceb5bf5cd07d
Liu-Steve <steveliu123@hotmail.com>
2026-01-10 20:16:11 +0800

    20260110

---
+++ 20260110-712.cpp
@@ -0,0 +1,86 @@
+#include "bits/stdc++.h"
+#include <vector>
+#define io                                                                     \
+  ios::sync_with_stdio(false);                                                 \
+  cin.tie(0);                                                                  \
+  cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+  int val;
+  vector<Node *> children;
+
+  Node() {}
+
+  Node(int _val) { val = _val; }
+
+  Node(int _val, vector<Node *> _children) {
+    val = _val;
+    children = _children;
+  }
+};
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+  int minimumDeleteSum(string s1, string s2) {
+    int n = s1.size();
+    int m = s2.size();
+    vector dp1(n + 1, 0), dp2(n + 1, 0);
+    for (int i = 0; i < n; ++i) {
+      dp1[i + 1] = dp1[i] + s1[i];
+    }
+    for (int i = 0; i < m; ++i) {
+      dp2[0] = dp1[0] + s2[i];
+      for (int j = 0; j < n; ++j) {
+        dp2[j + 1] = min(dp2[j] + s1[j], dp1[j + 1] + s2[i]);
+        if (s1[j] == s2[i]) {
+          dp2[j + 1] = min(dp2[j + 1], dp1[j]);
+        }
+      }
+      swap(dp1, dp2);
+    }
+    return dp1[n];
+  }
+};
+
+int main() {
+  io;
+  return 0;
+}
