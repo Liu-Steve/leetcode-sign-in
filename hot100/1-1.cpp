324a8bc956a6b129a0dd67a98030dbf60455021a
Liu-Steve <steveliu123@hotmail.com>
2026-02-01 21:56:14 +0800

    hot100 1

---
+++ hot100/1-1.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int, int> hm;
+        for (int i = 0; i < n; ++i) {
+            hm[nums[i]] = i;
+        }
+        for (int i = 0; i < n; ++i) {
+            if (hm.count(target - nums[i]) && hm[target - nums[i]] != i) {
+                return {i, hm[target - nums[i]]};
+            }
+        }
+        return {};
+    }
+};
+
+int main() {
+    io;
+    Solution s;
+    return 0;
+}
