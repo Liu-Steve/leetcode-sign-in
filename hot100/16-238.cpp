3e8aeb366e32796f8b4c7dc8927ee834b789fdd8
Liu-Steve <steveliu123@hotmail.com>
2026-02-02 19:38:18 +0800

    hot100 16

---
+++ hot100/16-238.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 1);
+        int pre = 1;
+        for (int i = 0; i < n; ++i) {
+            ans[i] *= pre;
+            pre *= nums[i];
+        }
+        int suf = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            ans[i] *= suf;
+            suf *= nums[i];
+        }
+        return ans;
+    }
+};
+
+int main() {
+    io;
+    Solution s;
+    return 0;
+}
