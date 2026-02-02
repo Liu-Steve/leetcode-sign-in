696c76e580f38b5d285a336d0026a2993f1378b3
Liu-Steve <steveliu123@hotmail.com>
2026-02-02 19:29:36 +0800

    hot100 14

---
+++ hot100/14-56.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        int l = intervals[0][0];
+        int r = intervals[0][1];
+        for (auto& in : intervals) {
+            if (in[0] > r) {
+                ans.push_back(vector<int>{l, r});
+                l = in[0];
+                r = in[1];
+                continue;
+            }
+            r = max(r, in[1]);
+        }
+        ans.push_back(vector<int>{l, r});
+        return ans;
+    }
+};
+
+int main() {
+    io;
+    Solution s;
+    return 0;
+}
