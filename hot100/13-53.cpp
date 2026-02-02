ec3af02d7f64c3b6f49a49c343084ff933a75f44
Liu-Steve <steveliu123@hotmail.com>
2026-02-02 19:23:41 +0800

    hot100 13

---
+++ hot100/13-53.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int minPre = 0;
+        int pre = 0;
+        int ans = nums[0];
+        for (int num : nums) {
+            pre += num;
+            ans = max(ans, pre - minPre);
+            minPre = min(minPre, pre);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    io;
+    Solution s;
+    return 0;
+}
