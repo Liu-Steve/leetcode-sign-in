9b555f21fe003c81c4fc7d77eab73b464ec63677
Liu-Steve <steveliu123@hotmail.com>
2026-02-02 22:28:17 +0800

    hot100 23

---
+++ hot100/23-206.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+        ListNode *p1 = head;
+        ListNode *p2 = head->next;
+        p1->next = nullptr;
+        while (p2 != nullptr) {
+            ListNode *p3 = p2->next;
+            p2->next = p1;
+            p1 = p2;
+            p2 = p3;
+        }
+        return p1;
+    }
+};
+
+int main() {
+    io;
+    Solution s;
+    return 0;
+}
