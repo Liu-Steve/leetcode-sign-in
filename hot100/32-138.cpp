424ee276a1d6cc45883bc982f9285ae27b8219b1
Liu-Steve <steveliu123@hotmail.com>
2026-02-05 23:40:27 +0800

    hot100 32

---
+++ hot100/32-138.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+// class Node {
+// public:
+//     int val;
+//     vector<Node*> children;
+
+//     Node() {}
+
+//     Node(int _val) {
+//         val = _val;
+//     }
+
+//     Node(int _val, vector<Node*> _children) {
+//         val = _val;
+//         children = _children;
+//     }
+// };
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+constexpr long long MOD = 1000000007LL;
+
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        unordered_map<Node*, Node*> hm;
+        hm[nullptr] = nullptr;
+        function<Node*(Node*)> dfs = [&](Node* node) -> Node* {
+            if (hm.count(node)) {
+                return hm[node];
+            }
+            Node* new_node = new Node(node->val);
+            hm[node] = new_node;
+            new_node->next = dfs(node->next);
+            new_node->random = dfs(node->random);
+            return new_node;
+        };
+        return dfs(head);
+    }
+};
+
+int main() {
+    io;
+    Solution s;
+    return 0;
+}
