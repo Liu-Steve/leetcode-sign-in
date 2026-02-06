cf59023b0328c6775dba516da4e27402aa4c6819
Liu-Steve <steveliu123@hotmail.com>
2026-02-06 22:06:16 +0800

    hot100 36

---
+++ hot100/36-94.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        function<void(TreeNode*, vector<int>&)> dfs = [&](TreeNode* root, vector<int>& ans) {
+            if (root == nullptr) {
+                return;
+            }
+            dfs(root->left, ans);
+            ans.push_back(root->val);
+            dfs(root->right, ans);
+        };
+        dfs(root, ans);
+        return ans;
+    }
+};
+
+int main() {
+    io;
+    return 0;
+}
