266f1eb2378d05caf947e93472bc454354ff924a
Liu-Steve <steveliu123@hotmail.com>
2026-02-06 22:43:34 +0800

    hot100 42

---
+++ hot100/42-108.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        function<TreeNode*(int, int)> dfs = [&](int l, int r) -> TreeNode* {
+            if (l >= r) {
+                return nullptr;
+            }
+            int m = (l + r) / 2;
+            TreeNode* ans = new TreeNode(nums[m]);
+            if (l + 1 == r) {
+                return ans;
+            }
+            ans->left = dfs(l, m);
+            ans->right = dfs(m + 1, r);
+            return ans;
+        };
+        return dfs(0, nums.size());
+    }
+};
+
+int main() {
+    io;
+    return 0;
+}
