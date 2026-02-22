b1b2978277a546593ba27e6d45e2384e992c4d26
Liu-Steve <steveliu123@hotmail.com>
2026-02-22 14:41:05 +0800

    20260222

---
+++ 20260222-868.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    int binaryGap(int n) {
+        int ans = 0;
+        int cnt = 0;
+        int lst = -1;
+        while (n) {
+            int num = n & 1;
+            n >>= 1;
+            cnt++;
+            if (num == 0) {
+                continue;
+            }
+            if (lst == -1) {
+                lst = cnt;
+                continue;
+            }
+            ans = max(ans, cnt - lst);
+            lst = cnt;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    io;
+    Solution s;
+    return 0;
+}
