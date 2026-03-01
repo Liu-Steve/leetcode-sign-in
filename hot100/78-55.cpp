d70fbb1ee019baa040a337a7d733b9a0c3860a22
Liu-Steve <steveliu123@hotmail.com>
2026-03-01 21:11:11 +0800

    hot100 78

---
+++ hot100/78-55.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int idx = 0;
+        for (int i = 0; i <= idx; ++i) {
+            idx = max(idx, i + nums[i]);
+            if (idx >= nums.size() - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    io;
+    return 0;
+}
