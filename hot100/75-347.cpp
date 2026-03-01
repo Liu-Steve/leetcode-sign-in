b5060d86a9052fb8931b0ab5e0ea6ed1a4608ab2
Liu-Steve <steveliu123@hotmail.com>
2026-03-01 16:34:10 +0800

    hot100 75

---
+++ hot100/75-347.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> cnt;
+        for (int num : nums) {
+            cnt[num]++;
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+        for (auto it = cnt.begin(); it != cnt.end(); ++it) {
+            pq.emplace(it->second, it->first);
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+        vector<int> ans;
+        while (!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
+
+int main() {
+    io;
+    return 0;
+}
