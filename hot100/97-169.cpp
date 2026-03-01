b166ecf558ec4a0ac98e281564f1a6c8ff679455
Liu-Steve <steveliu123@hotmail.com>
2026-03-01 22:16:07 +0800

    hot100 97

---
+++ hot100/97-169.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ans = nums.front();
+        int cnt = 1;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] == ans) {
+                cnt++;
+                continue;
+            }
+            cnt--;
+            if (cnt <= 0) {
+                ans = nums[i];
+                cnt = 1;
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    io;
+    return 0;
+}
