fceb50d21b87e7d24788e525f99ba8045d0f6184
Liu-Steve <steveliu123@hotmail.com>
2026-03-01 21:39:24 +0800

    hot100 80

---
+++ hot100/80-763.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> ch(26);
+        for (int i = 0; i < s.size(); ++i) {
+            ch[s[i] - 'a'] = i;
+        }
+        vector<int> ans;
+        int r = 0;
+        for (int i = 0; i < s.size();) {
+            r = ch[s[i] - 'a'];
+            int j = i;
+            for (; j <= r; ++j) {
+                r = max(r, ch[s[j] - 'a']);
+            }
+            ans.push_back(j - i);
+            i = j;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    io;
+    return 0;
+}
