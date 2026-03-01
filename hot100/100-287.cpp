2c015b1305e1e031956c66c6af322a8d44d5f6a8
Liu-Steve <steveliu123@hotmail.com>
2026-03-01 23:31:02 +0800

    hot100 100

---
+++ hot100/100-287.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int fast = 0;
+        int slow = 0;
+        while (true) {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+            if (fast == slow) {
+                int target = 0;
+                while (target != slow) {
+                    target = nums[target];
+                    slow = nums[slow];
+                }
+                break;
+            }
+        }
+        return slow;
+    }
+};
+
+int main() {
+    io;
+    return 0;
+}
