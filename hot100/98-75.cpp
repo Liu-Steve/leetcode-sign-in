89ed0f86b713fbdbbcfa7679bf2e1c4c6ab07887
Liu-Steve <steveliu123@hotmail.com>
2026-03-01 22:17:37 +0800

    hot100 98

---
+++ hot100/98-75.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size() - 1;
+        for (int i = 0; i <= r; ++i) {
+            while (i <= r && nums[i] == 2) {
+                swap(nums[i], nums[r]);
+                r--;
+            }
+            if (nums[i] == 0) {
+                swap(nums[i], nums[l]);
+                l++;
+            }
+        }
+    }
+};
+
+int main() {
+    io;
+    return 0;
+}
