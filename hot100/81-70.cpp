cb013b013ae4ffc6f80907453a1b299fac7ca7fd
Liu-Steve <steveliu123@hotmail.com>
2026-03-04 17:35:04 +0800

    hot100 81

---
+++ hot100/81-70.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int lst1 = 1;
+        int lst2 = 0;
+        for (int i = 1; i <= n; ++i) {
+            int ans = lst1 + lst2;
+            lst2 = lst1;
+            lst1 = ans;
+        }
+        return lst1;
+    }
+};
+
+int main() {
+    io;
+    return 0;
+}
