83969a70018fbbfabcd67fb6ec906cc99ce10036
Liu-Steve <steveliu123@hotmail.com>
2026-03-09 20:59:47 +0800

    hot100 87

---
+++ hot100/87-300.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> g;
+        for (int num : nums) {
+            auto it = lower_bound(g.begin(), g.end(), num);
+            if (it == g.end()) {
+                g.push_back(num);
+            } else {
+                *it = num;
+            }
+        }
+        return g.size();
+    }
+};
+
+int main() {
+    io;
+    return 0;
+}
