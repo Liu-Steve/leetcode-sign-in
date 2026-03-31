cb8ef3da58920ef342aae416f88e979192728092
Liu-Steve <steveliu123@hotmail.com>
2026-03-31 23:21:03 +0800

    20260331

---
+++ 20260331-3474.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    string generateString(string str1, string str2) {
+        int n = str1.size();
+        int m = str2.size();
+        string ans(n + m - 1, ' ');
+        for (int i = 0; i < n; ++i) {
+            if (str1[i] == 'F') {
+                continue;
+            }
+            for (int j = 0; j < m; ++j) {
+                if (ans[i + j] != ' ' && ans[i + j] != str2[j]) {
+                    return "";
+                }
+                ans[i + j] = str2[j];
+            }
+        }
+        string old_ans = ans;
+        for (char& c : ans) {
+            if (c == ' ') {
+                c = 'a';
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (str1[i] != 'F') {
+                continue;
+            }
+            if (string(ans.begin() + i, ans.begin() + i + m) != str2) {
+                continue;
+            }
+            bool ok = false;
+            for (int j = i + m - 1; j >= i; j--) {
+                if (old_ans[j] == ' ') {
+                    ans[j] = 'b';
+                    ok = true;
+                    break;
+                }
+            }
+            if (!ok) {
+                return "";
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    io;
+    return 0;
+}
