879fc9f453aa0c0b0506f8f2ba02d9a2be690172
Liu-Steve <steveliu123@hotmail.com>
2026-04-11 15:47:23 +0800

    20260411

---
+++ 20260411-3741.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        int ans = INT32_MAX;
+        unordered_map<int, vector<int>> pos;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (pos.count(nums[i]) && pos[nums[i]].size() != 1) {
+                ans = min(ans, 2 * (i - pos[nums[i]].front()));
+                pos[nums[i]].erase(pos[nums[i]].begin());
+            }
+            pos[nums[i]].push_back(i);
+        }
+        return ans == INT32_MAX ? -1 : ans;
+    }
+};
+
+int main() {
+    io;
+    return 0;
+}
