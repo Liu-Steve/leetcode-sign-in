a94ba4c7c333a4b8cca04c6f61a1ec6419c2174f
Liu-Steve <steveliu123@hotmail.com>
2026-04-26 00:12:53 +0800

    20260425

---
+++ 20260425-3464.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    int maxDistance(long long side, vector<vector<int>>& points, int k) {
+        int n = points.size();
+        vector<long long> step(n);
+        for (int i = 0; i < n; ++i) {
+            auto& p = points[i];
+            long long t;
+            if (p[1] == 0) {
+                t = p[0];
+            } else if (p[0] == side) {
+                t = p[1] + side;
+            } else if (p[1] == side) {
+                t = side - p[0] + side * 2;
+            } else {
+                t = side - p[1] + side * 3;
+            }
+            step[i] = t;
+        }
+        sort(step.begin(), step.end());
+        auto valid = [&](int dis) -> bool {
+            for (long long start : step) {
+                long long end = start + side * 4ll - dis;
+                long long cur = start;
+                for (int i = 0; i < k - 1; ++i) {
+                    auto it = lower_bound(step.begin(), step.end(), cur + dis);
+                    if (it == step.end() || *it > end) {
+                        cur = -1;
+                        break;
+                    }
+                    cur = *it;
+                }
+                if (cur >= 0) {
+                    return true;
+                }
+            }
+            return false;
+        };
+        int l = 1;
+        int r = side + 1;
+        while (l + 1 < r) {
+            int m = (l + r) / 2;
+            if (valid(m)) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        return l;
+    }
+};
+
+int main() {
+    io;
+    return 0;
+}
