c6a17a8876efce731d7de4d159210e3abd456cb0
Liu-Steve <steveliu123@hotmail.com>
2026-05-01 23:01:08 +0800

    20260501

---
+++ 20260501-396.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+
+#include <vector>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int sum = 0;
+        int cnt = 0;
+        int ans = INT32_MIN;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            sum += nums[i];
+            cnt += nums[i] * i;
+        }
+        for (int i = 0; i < n; ++i) {
+            cnt += nums[i] * n;
+            cnt -= sum;
+            ans = max(ans, cnt);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    io;
+    return 0;
+}
