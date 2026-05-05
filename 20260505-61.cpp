69841e202cc63561e118ed2284d52a394a50f691
Liu-Steve <steveliu123@hotmail.com>
2026-05-05 10:30:47 +0800

    20260505

---
+++ 20260505-61.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+#include <vector>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+        ListNode* tmp = head;
+        int n = 0;
+        while (true) {
+            n++;
+            if (tmp->next == nullptr) {
+                tmp->next = head;
+                break;
+            }
+            tmp = tmp->next;
+        }
+        tmp = head;
+        k = n - k % n;
+        for (int i = 1; i < k; ++i) {
+            tmp = tmp->next;
+        }
+        ListNode* ans = tmp->next;
+        tmp->next = nullptr;
+        return ans;
+    }
+};
+
+int main() {
+    io;
+    return 0;
+}
