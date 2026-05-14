2075a9f17316e12774d982fd508c4ab014e19221
Liu-Steve <steveliu123@hotmail.com>
2026-05-15 00:04:22 +0800

    20260515

---
+++ 20260515-153.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+
+#include <vector>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        if (nums.front() <= nums.back()) {
+            return nums.front();
+        }
+        int l = 0;
+        int r = nums.size() - 1;
+        while (l + 1 < r) {
+            int m = (l + r) / 2;
+            if (nums[m] > nums[l]) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        return nums[r];
+    }
+};
+
+int main() {
+    io;
+    return 0;
+}
