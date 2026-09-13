e2e81a95e9509b25cf602a62bcc5843a69cd7306
Liu-Steve <steveliu123@hotmail.com>
2026-09-13 22:45:43 +0800

    20260913

---
+++ 20260913-835.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+
+#include <vector>
+#define io                       \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vl> vvl;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef pair<int, int> pr;
+typedef pair<ll, ll> prl;
+typedef vector<pr> vp;
+typedef unordered_set<int> hm;
+typedef unordered_map<ll, int> memory;
+
+constexpr long long MOD = 1000000007LL;
+
+class Solution {
+public:
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        int n = img1.size();
+        int ans = 0;
+        for (int i = 1 - n; i <= n - 1; ++i) {
+            for (int j = 1 - n; j <= n - 1; ++j) {
+                int sum = 0;
+                for (int x = max(0, i); x < min(n, n + i); ++x) {
+                    for (int y = max(0, j); y < min(n, n + j); ++y) {
+                        sum += (img1[x][y] && img2[x - i][y - j]);
+                    }
+                }
+                ans = max(ans, sum);
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    io;
+    return 0;
+}
